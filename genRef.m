23648844e35692b8834c486e4f20dda41a37f355
thatmooglie <bragimarinosson@gmail.com>
2020-01-20 10:14:58 +0100

    bragi- fbcca,pca,channel selection, NOTWORKING

--- genRef.m
+++ genRef.m
@@ -1,11 +1,11 @@
-function y = genRef(nH, f)
+function y = genRef(nH, f, N)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-y = zeros(40, 1250, 2*nH);
+y = zeros(40, N, 2*nH);
 for i=1:40
     for k=1:nH
-        y(i,:,2*k-1) = sin(2*pi*f(i)*k*(0:1250-1)*1/250);
-        y(i,:,2*k) = cos(2*pi*f(i)*k*(0:1250-1)*1/250);
+        y(i,:,2*k-1) = sin(2*pi*f(i)*k*(0:N-1)*1/250);
+        y(i,:,2*k) = cos(2*pi*f(i)*k*(0:N-1)*1/250);
     end
 end
 end
